20b9af4c777dfde07a9f845cb8e4fd34dcf1088f
nicolasluckert <nicolas.luckert@web.de>
2023-11-12 00:56:25 +0200

    created basic scene for developing scooter upgrades

---
+++ src/Assets/Scripts/RotationDisplay.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationDisplay : MonoBehaviour
+{
+    public float RotationSpeed;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ src/Assets/Scripts/UpgradeScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeScreen : MonoBehaviour
+{
+    public void OnSpeedUpgradeClick()
+    {
+        Debug.Log("Vroooom vroooom");
+    }
+
+    public void OnBatteryUpgradeClick()
+    {
+        Debug.Log("Powerbank docked");
+    }
+}
