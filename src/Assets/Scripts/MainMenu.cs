c331b26cda40fd6659cfbc28208ae16145416a9a
triczan <tristanczaraasmae@gmail.com>
2023-12-30 19:31:31 +0200

    Menus

--- src/Assets/Scripts/MainMenu.cs
+++ src/Assets/Scripts/MainMenu.cs
@@ -13,15 +13,38 @@ public class MainMenu : MonoBehaviour
 
     public TextMeshProUGUI Play;
 
+    public static int sceneLoadedNum = 0;
+
 
     private void Awake()
     {
         InstructionsPanel.SetActive(false);
         Story.SetActive(false);
         Keys.SetActive(false);
+
+        sceneLoadedNum++;
     }
+
     public void PlayGame()
     {
+        SoundController.SoundInstance.ButtonClick();
+        if (sceneLoadedNum < 2)
+        {
+            InstructionsPanel.SetActive(true);
+            Story.SetActive(true);
+            Keys.SetActive(false);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Play");
+            SceneManager.LoadSceneAsync(1);
+        }
+       
+    }
+
+    public void PlayButton()
+    {
+        SoundController.SoundInstance.ButtonClick();
         UnityEngine.Debug.Log("Play");
         SceneManager.LoadSceneAsync(1);
     }
@@ -29,21 +52,38 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
+        SoundController.SoundInstance.ButtonClick();
         Application.Quit();
         UnityEngine.Debug.Log("Quit the game");
     }
 
     public void Instructions()
     {
-        InstructionsPanel.SetActive(true);
-        Story.SetActive(true);
-        Keys.SetActive(false);
+        SoundController.SoundInstance.ButtonClick();
+        if (sceneLoadedNum < 2)
+        {
+            InstructionsPanel.SetActive(true);
+            Story.SetActive(true);
+            Keys.SetActive(false);
+        }
+        else
+        {
+            Next();
+        }
+        
     }
 
     public void Next()
     {
+        SoundController.SoundInstance.ButtonClick();
         InstructionsPanel.SetActive(true);
         Story.SetActive(false);
         Keys.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        SoundController.SoundInstance.ButtonClick();
+        SceneManager.LoadSceneAsync("MainMenu");
     }
 }
